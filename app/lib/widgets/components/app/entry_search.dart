0ea7abd4451eed97b4394699b839aebf79708a8c
jhqwqmc <2110242767@qq.com>
2024-08-25 21:43:39 +0800

    修复

--- app/lib/widgets/components/app/entry_search.dart
+++ app/lib/widgets/components/app/entry_search.dart
@@ -94,7 +94,7 @@ Fuzzy<EntryDefinition> _fuzzyEntries(_FuzzyEntriesRef ref) {
         blueprint: blueprint,
         entry: entry,
       );
-    }).nonNulls();
+    }).where((element) => element != null);
   }).toList();
 
   return Fuzzy(
